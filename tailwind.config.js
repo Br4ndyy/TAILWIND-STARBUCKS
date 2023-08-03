6339c7b889d73f066e0e24cf4cae849f0bb3ea41
Emmszhii <emmanuelmanalo707@gmail.com>
2023-08-03 15:58:50 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors:{
+        'puti':'#ffffff',
+        'maitim':'#2b2b2b',
+      }
+    },
+  },
+  plugins: [],
+}
+
