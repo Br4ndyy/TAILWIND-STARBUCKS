3addb1a43baa52fbbb3a85a0e2b90e9f22450ea9
Emmszhii <emmanuelmanalo707@gmail.com>
2023-08-04 15:44:28 +0800

    fave

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
         'puti':'#ffffff',
         'maitim':'#2b2b2b',
         'lasalle':'#006341',
-        'semento':'hsl(240,1.51%,50.98%)'
+        'semento':'hsl(240,1.51%,50.98%)',
+        'medyolasalle':'#f1f8f5',
+        'medyolasalle2':'#d4e9e2',
       }
     },
   },
