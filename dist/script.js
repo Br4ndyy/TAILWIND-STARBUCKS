a740ffc1445b0a745d41629030adeda11a40957e
unknown <brandon_mark_cantos@dlsl.edu.ph>
2023-08-06 00:03:39 +0800

    for completion

--- dist/script.js
+++ dist/script.js
@@ -24,6 +24,44 @@
 //     });
 // }
 
+const faveNav = document.querySelectorAll(".fave__nav__link");
+const faveContent = document.querySelectorAll(".fave__item__content");
+faveNav.forEach((fave) => {
+    fave.addEventListener("click", () => {
+        removeActiveFave();
+        fave.style = "transition:.5s ease";
+        fave.classList.add("active");
+        fave.style = "transition:.5s ease";
+        const activeContent = document.querySelector(`#${fave.id}-content`);
+        removeActiveContent();
+        activeContent.classList.add("active");
+    });
+});
+
+// activeLin.forEach((lin) => {
+//     lin.addEventListener("click", () => {
+//         removeActiveFave();
+//         lin.style = "transition:.5s ease";
+//         lin.classList.add("active");
+//         lin.style = "transition:.5s ease";
+//         const activeContent = document.querySelectorAll(`#${fave.id}-lin`);
+//         removeActiveContent();
+//         activeContent.classList.add("active");
+//     });
+// });
+
+function removeActiveFave() {
+    faveNav.forEach((fave) => {
+        fave.classList.remove("active");
+    });
+}
+
+function removeActiveContent() {
+    faveContent.forEach((fave) => {
+        fave.classList.remove("active");
+    });
+}
+
 const headerNav = document.querySelector('.header__nav');
 const toggleMenu = document.querySelector('.toggle__menu');
 const hokbu = document.querySelector('.buhok');
