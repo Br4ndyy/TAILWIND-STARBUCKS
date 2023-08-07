0d8399357e5533f40f3b50a73f399298a6f32a6b
Emmszhii <emmanuelmanalo707@gmail.com>
2023-08-07 15:55:38 +0800

    for completion til wed

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
 				semento: 'hsl(240,1.51%,50.98%)',
 				medyolasalle: '#f1f8f5',
 				medyolasalle2: '#d4e9e2',
+				beige: '#F2F0EB',
 			},
 			backgroundImage: {
 				star: "url('./img/rewards/star.png')",
