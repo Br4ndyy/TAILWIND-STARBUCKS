72e61425d7a503470f04f579d0bb1f6600da9503
Emmszhii <emmanuelmanalo707@gmail.com>
2023-08-07 13:00:30 +0800

    for completion

--- dist/script.js
+++ dist/script.js
@@ -1,42 +1,42 @@
-const kalboNav = document.querySelectorAll(".kalbo__nav__link");
-const kalboContent = document.querySelectorAll(".kalbo__item__content");
-kalboNav.forEach((kalbo) => {
-    kalbo.addEventListener("click", () => {
-        removeActiveKalbo();
-        kalbo.style = "transition:.5s ease";
-        kalbo.classList.add("active");
-        kalbo.style = "transition:.5s ease";
-        const activeContent = document.querySelector(`#${kalbo.id}-content`);
-        removeActiveContent();
-        activeContent.classList.add("active");
-    });
-});
-
-function removeActiveKalbo() {
-    kalboNav.forEach((kalbo) => {
-        kalbo.classList.remove("active");
-    });
-}
-
-function removeActiveContent() {
-    kalboContent.forEach((kalbo) => {
-        kalbo.classList.remove("active");
-    });
-}
-
-// const faveNav = document.querySelectorAll(".fave__nav__link");
-// const faveContent = document.querySelectorAll(".fave__item__content");
-// faveNav.forEach((fave) => {
-//     fave.addEventListener("click", () => {
-//         removeActiveFave();
-//         fave.style = "transition:.5s ease";
-//         fave.classList.add("active");
-//         fave.style = "transition:.5s ease";
-//         const activeContent = document.querySelector(`#${fave.id}-content`);
+// const kalboNav = document.querySelectorAll(".kalbo__nav__link");
+// const kalboContent = document.querySelectorAll(".kalbo__item__content");
+// kalboNav.forEach((kalbo) => {
+//     kalbo.addEventListener("click", () => {
+//         removeActiveKalbo();
+//         kalbo.style = "transition:.5s ease";
+//         kalbo.classList.add("active");
+//         kalbo.style = "transition:.5s ease";
+//         const activeContent = document.querySelector(`#${kalbo.id}-content`);
 //         removeActiveContent();
 //         activeContent.classList.add("active");
 //     });
 // });
+
+// function removeActiveKalbo() {
+//     kalboNav.forEach((kalbo) => {
+//         kalbo.classList.remove("active");
+//     });
+// }
+
+// function removeActiveContent() {
+//     kalboContent.forEach((kalbo) => {
+//         kalbo.classList.remove("active");
+//     });
+// }
+
+const faveNav = document.querySelectorAll('.fave__nav__link');
+const faveContent = document.querySelectorAll('.fave__item__content');
+faveNav.forEach((fave) => {
+	fave.addEventListener('click', () => {
+		removeActiveFave();
+		fave.style = 'transition:.5s ease';
+		fave.classList.add('active');
+		fave.style = 'transition:.5s ease';
+		const activeContent = document.querySelector(`#${fave.id}-content`);
+		removeActiveContent();
+		activeContent.classList.add('active');
+	});
+});
 
 // activeLin.forEach((lin) => {
 //     lin.addEventListener("click", () => {
@@ -50,17 +50,17 @@ function removeActiveContent() {
 //     });
 // });
 
-// function removeActiveFave() {
-//     faveNav.forEach((fave) => {
-//         fave.classList.remove("active");
-//     });
-// }
+function removeActiveFave() {
+	faveNav.forEach((fave) => {
+		fave.classList.remove('active');
+	});
+}
 
-// function removeActiveContent() {
-//     faveContent.forEach((fave) => {
-//         fave.classList.remove("active");
-//     });
-// }
+function removeActiveContent() {
+	faveContent.forEach((fave) => {
+		fave.classList.remove('active');
+	});
+}
 
 const headerNav = document.querySelector('.header__nav');
 const toggleMenu = document.querySelector('.toggle__menu');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist/**/*.{html,js}'],
-  theme: {
-    extend: {
-      colors:{
-        'puti':'#ffffff',
-        'maitim':'#2b2b2b',
-        'lasalle':'#006341',
-        'semento':'hsl(240,1.51%,50.98%)',
-        'medyolasalle':'#f1f8f5',
-        'medyolasalle2':'#d4e9e2',
-      },
-      backgroundImage:{
-        'star':"url('./img/rewards/star.png')"
-      }
-    },
-  },
-  plugins: [],
-}
-
+	content: ['./dist/**/*.{html,js}'],
+	theme: {
+		extend: {
+			colors: {
+				puti: '#ffffff',
+				maitim: '#2b2b2b',
+				lasalle: '#006341',
+				semento: 'hsl(240,1.51%,50.98%)',
+				medyolasalle: '#f1f8f5',
+				medyolasalle2: '#d4e9e2',
+			},
+			backgroundImage: {
+				star: "url('./img/rewards/star.png')",
+			},
+		},
+	},
+	plugins: [],
+};
